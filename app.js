41cde3999838f7c6bf20d0d8896ff848b80958b9
Xchel_Sanchez <xchelsvz@gmail.com>
2015-01-22 23:12:17 -0600

    Empezando Proyecto

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', './views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(__dirname + 'public'));
+
+
+
